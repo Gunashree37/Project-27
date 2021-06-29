22f961d838aa06a211cf56c9d4e4b707933f249f
Gunashree S <gunashrees341@gmail.com>
2021-06-29 16:48:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
+var rope1,rope2,rope3,rope4,rope5,roof;
 
 
 
@@ -13,6 +14,23 @@ function setup() {
 
 	engine = Engine.create();
 	world = engine.world;
+	
+	roof=new Roof(500,100,600,20)
+	
+	//create the bobs here
+	
+	
+	
+	
+	
+	
+	
+	rope1=new Rope(bob1.body,roof.body,100)
+	rope2=new Rope(bob2.body,roof.body,150)
+	rope3=new Rope(bob3.body,roof.body,200)
+	rope4=new Rope(bob4.body,roof.body,250)
+	rope5=new Rope(bob5.body,roof.body,300)
+
 
 	
 
@@ -23,6 +41,18 @@ function setup() {
 
 function draw() {
   background("pink");
+  roof.display();
+	
+	
+	
+	
+	
+	
+ rope1.display();
+ rope2.display();
+ rope3.display();
+ rope4.display();
+ rope5.display();
   
   
  
